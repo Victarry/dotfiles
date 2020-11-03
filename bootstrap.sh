557f6d47c827fbeba4629e4deaac7b5978f9623e
Zhenhuan Liu <nkulzh16@gmail.com>
2020-11-03 19:54:55 +0800

    Add conda install script. close #2
    1. Conda install script download and silent install.
    2. Condarc change for tsinghua tuna mirrors.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,42 @@ function doIt() {
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
 }
+
+
+if [ ! -d "$HOME/anaconda3" ]; then
+    CONDA_FILE=Anaconda3-2020.07-Linux-x86_64.sh
+    wget "https://repo.anaconda.com/archive/$CONDA_FILE" -O $HOME/$CONDA_FILE
+    bash $HOME/$CONDA_FILE -b -p $HOME/anaconda3
+
+    cat >> $HOME/.extra << EOL
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="\$('$HOME/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ \$? -eq 0 ]; then
+    eval "\$__conda_setup"
+else
+    if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/anaconda3/bin:\$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+EOL
+    rm $HOME/$CONDA_FILE
+
+    cat >> $HOME/.condarc << EOL
+channels:
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+  - defaults
+show_channel_urls: true
+EOL
+fi
+
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
